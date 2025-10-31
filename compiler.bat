43998561ac1641345b6c5840f7a807ba11c3cde8
Javier <javier.ortin.rodenas@gmail.com>
2025-10-31 22:16:38 +0100

    Ejercicio hecho aleluya

--- compiler.bat
+++ compiler.bat
@@ -1,17 +1,24 @@
 @echo off
-copy "D:\UNIOVI\latex-compiler\3Carrera\AMat3\apuntes.tex" "%cd%\comp.tex"
+setlocal EnableDelayedExpansion
+
+:: CONFIG
+set "FOLDER=3Carrera\AMat3\Ejercicios"
+set "FILENAME=ejercicios"
+set "COMP_FILE=comp.tex"
+
+:: PROGRAM
+copy "%FOLDER%\%FILENAME%.tex" "%cd%\%COMP_FILE%"
 cls
 :loop
-fc /b "D:\UNIOVI\latex-compiler\3Carrera\AMat3\apuntes.tex" "%cd%\comp.tex" >nul
+fc /b "%cd%\%FOLDER%\%FILENAME%.tex" "%cd%\%COMP_FILE%" >nul
 if errorlevel 1 (
     ::no son iguales
     cls
-    copy "D:\UNIOVI\latex-compiler\3Carrera\AMat3\apuntes.tex" "%cd%\comp.tex"
-    call "C:\Program Files\Git\bin\bash.exe" -c "cd D:/UNIOVI/latex-compiler; ls;./laton -d https://latexonline.cc ./3Carrera/AMat3/apuntes.tex"
-    copy "%cd%\apuntes.pdf" "D:\UNIOVI\latex-compiler\3Carrera\AMat3\apuntes.pdf"
+    copy "%cd%\%FOLDER%\%FILENAME%.tex" "%cd%\%COMP_FILE%"
+    call "C:\Program Files\Git\bin\bash.exe" -c "cd %cd:\=/%; ls;./laton -d https://latexonline.cc ./%FOLDER:\=/%/%FILENAME%.tex"
+    copy "%cd%\%FILENAME%.pdf" "%cd%\%FOLDER%\%FILENAME%.pdf"
     echo Compilado
 ) else (
     timeout /t 5 > nul
 )
-
 goto :loop
