7401fe034d2aa2a60c4e8c6743ad81118fda63c0
JavierOrtin <javier.ortin.rodenas@gmail.com>
2026-03-06 13:19:47 +0100

    Entrega hecha

--- 3Carrera/SO/V1/code/6b.c
+++ 3Carrera/SO/V1/code/6b.c
@@ -8,6 +8,6 @@ while (OperatingSystem_IsThereANewProgram()!=EMPTYQUEUE) {
   createdProcessPID=OperatingSystem_CreateProcess(i);
   switch (createdProcessPID) {
     case NOFREEENTRY:	// New case (modification)
-	   (TIMED_MESSAGE, 51, ERROR, programList[i]->executableName);
+	   (TIMED_MESSAGE, 50, ERROR, programList[i]->executableName);
 	   break;
 // Rest of the function...
